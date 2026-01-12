7722407c377eb0c0e63b8b4cea7d33725b6e15d7
Ali Haider <ha4219466@gmail.com>
2026-01-12 23:32:13 +0500

    Add problem descriptions to SQL files for selecting Japanese cities

--- sql/hackerrank/basic/select-japanese-cities-attributes.sql
+++ sql/hackerrank/basic/select-japanese-cities-attributes.sql
@@ -1,3 +1,4 @@
+-- Problem: Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
 /*
     Enter your query here and follow these instructions:
     1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
--- sql/hackerrank/basic/select-japanese-cities-names.sql
+++ sql/hackerrank/basic/select-japanese-cities-names.sql
@@ -1,3 +1,4 @@
+-- Problem: Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
 /*
     Enter your query here and follow these instructions:
     1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
